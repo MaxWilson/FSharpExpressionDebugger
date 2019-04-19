99c4a3d54f24f62b5b27c9a57a8e0add27397772
Max Wilson <wilson.max@gmail.com>
2019-04-19 08:47:46 -0700

    Small refactor: rename Tracker

--- FSharpDebugging/ExpressionCompiler.fs
+++ FSharpDebugging/ExpressionCompiler.fs
@@ -7,7 +7,7 @@ open Microsoft.VisualStudio.Debugger
 open Microsoft.VisualStudio.Debugger.Evaluation
 
 // used to track whether this expression is a synthetic expression intended to go to C#
-type Tracker() =
+type CSharpExpressionMarker() =
     inherit DkmDataItem()
 
 type ExpressionCompiler() = 
@@ -15,13 +15,15 @@ type ExpressionCompiler() =
         member this.CompileExpression(expr, addr, ctx, error, result) = 
             if expr.Text = "x" then
                 error <- "Hello world from F# v0.1."
-            elif expr.Text = "y" && expr.GetDataItem<Tracker>() |> box = null then
+            elif expr.GetDataItem<CSharpExpressionMarker>() |> box <> null then
+                // if CSharpExpressionMarker is attached then the expression has already been rewritten: evaluate in C#
+                expr.CompileExpression(addr, ctx, &error, &result)
+            elif expr.Text = "y" then
                 // evaluate "42" in C#
-                let e = DkmLanguageExpression.Create(expr.Language, expr.CompilationFlags, "42*42", Tracker())
+                let e = DkmLanguageExpression.Create(expr.Language, expr.CompilationFlags, "42*42", CSharpExpressionMarker())
                 e.CompileExpression(addr, ctx, &error, &result)
             else
-                // fall through
-                expr.CompileExpression(addr, ctx, &error, &result)
+                error <- "Not implemented"
         member this.CompileAssignment(expr, addr, ctx, error, result) =
             expr.CompileAssignment(addr, ctx, &error, &result)
         member this.GetClrLocalVariableQuery(expr, addr, argsOnly) = 
