6cd48e46d46c2638f4a187b9777c19aa3844685c
Max Wilson <wilson.max@gmail.com>
2019-04-21 09:40:48 -0700

    Fix warnings, add Thunk.cs

---
+++ FSharpDebuggingExtension/Thunk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSharpDebuggingExtension
+{
+    // This class does nothing, exists just to get helpful C#-style F12s during development
+    class Thunk
+    {
+        
+    }
+}
