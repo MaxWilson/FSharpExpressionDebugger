1d4df0fd64fd97a828c009f6a18facf5fbccac61
Max Wilson <wilson.max@gmail.com>
2019-04-22 08:12:07 -0700

    Tutorials

--- FSharpDebugging/ExpressionCompiler.fs
+++ FSharpDebugging/ExpressionCompiler.fs
@@ -5,10 +5,7 @@ open Microsoft.VisualStudio.Debugger.ComponentInterfaces
 open System.Runtime.InteropServices
 open Microsoft.VisualStudio.Debugger
 open Microsoft.VisualStudio.Debugger.Evaluation
-
-// used to track whether this expression is a synthetic expression intended to go to C#
-type CSharpExpressionMarker() =
-    inherit DkmDataItem()
+open Translator
 
 type ExpressionCompiler() = 
     interface IDkmClrExpressionCompiler with
@@ -18,12 +15,10 @@ type ExpressionCompiler() =
             elif expr.GetDataItem<CSharpExpressionMarker>() |> box <> null then
                 // if CSharpExpressionMarker is attached then the expression has already been rewritten: evaluate in C#
                 expr.CompileExpression(addr, ctx, &error, &result)
-            elif expr.Text = "y" then
-                // evaluate "42" in C#
-                let e = DkmLanguageExpression.Create(expr.Language, expr.CompilationFlags, "42*42", CSharpExpressionMarker())
-                e.CompileExpression(addr, ctx, &error, &result)
             else
-                error <- "Not implemented"
+                match Translator.translateCSharp expr addr ctx with
+                | Error e -> error <- e
+                | Ok r -> result <- r
         member this.CompileAssignment(expr, addr, ctx, error, result) =
             expr.CompileAssignment(addr, ctx, &error, &result)
         member this.GetClrLocalVariableQuery(expr, addr, argsOnly) = 
--- FSharpDebuggingExtension/Thunk.cs
+++ FSharpDebuggingExtension/Thunk.cs
@@ -9,6 +9,11 @@ namespace FSharpDebuggingExtension
     // This class does nothing, exists just to get helpful C#-style F12s during development
     class Thunk
     {
-        
+        public void Test()
+        {
+            Func<int, int> f = a => -a;
+            var vs = Microsoft.FSharp.Collections.ListModule.OfSeq<int>(new[] { 1, 2, 3 });
+            Microsoft.FSharp.Collections.ListModule.Map<int, int>(Microsoft.FSharp.Core.FuncConvert.FromFunc<int, int>(v => f.Invoke(v)), vs);
+        }
     }
 }
